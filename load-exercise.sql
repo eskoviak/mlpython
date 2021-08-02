ac3ccab25a7c86e54fd6b2c5059e88af2d184d1c
Ed Skoviak <eskoviak@eskoviak.net>
2021-08-02 10:17:40 -0500

    updated loadfile and sql loader

--- load-exercise.sql
+++ load-exercise.sql
@@ -1,29 +1,35 @@
 create table if not exists Exercises (
     id              int auto_increment primary key,
-    exerciseName    varchar(40) not null
+    exerciseName    varchar(40) not null,
+	description		text null
 );
 
 truncate table Exercises;
 
-insert into Exercises (exerciseName)
-values ('Bulgarian Split Squat'),
-('Overhead BB Press'),
-('Pendlay Row'),
-('SL KB RDL'),
-('Deadlift'),
-('Zercher Sq'),
-('Kneeling Landmine Pr'),
-('SA Bench Row'),
-('Farmers Carry'),
-('Bench Press'),
-('Front Squat'),
-('EZ Bicep Curl'),
-('SA Farmer Carry'),
-('Rear Squat'),
-('Power Clean'),
-('Rear Deltoid'),
-('Hanging Leg Raise'),
-('Sumo Deadlift'),
-('Pull Up'),
-('Dips'),
-('Weighted Step Up');
+insert into Exercises (exerciseName, description)
+values ('Bulgarian Split Squat',''),
+('Overhead BB Press','')
+('Pendlay Row',''),
+('SL KB RDL',''),
+('Deadlift',''),
+('Zercher Sq',''),
+('Kneeling Landmine Pr',''),
+('SA Bench Row',''),
+('Farmers Carry',''),
+('Bench Press',''),
+('Front Squat',''),
+('EZ Bicep Curl',''),
+('SA Farmer Carry',''),
+('Rear Squat',''),
+('Power Clean',''),
+('Rear Deltoid',''),
+('Hanging Leg Raise',''),
+('Sumo Deadlift',''),
+('Pull Up',''),
+('Dips',''),
+('Weighted Step Up',''),
+('Ladder - Clean/Press','This is performed by doing 1 clean, followed by doing the specified press (string, push or snatch)'),
+('Two Hand KB Swing',''),
+('Ladder -- Goblet Squat',''),
+('Floor Press','')
+;
--- load-table.sql
+++ load-table.sql
@@ -2,5 +2,16 @@ load data local infile 'loadfile.csv'
   into table WeightLifting
         fields terminated by ',' optionally enclosed by '"'
         ignore 1 lines
-	(Date,ExerciseId,CategoryId,Cadence,Weights,WeightUnit,Comments)
+	(
+	Date,
+	@exerciseName,
+	@categoryName,
+	Cadence,
+	Weights,
+	WeightUnit,
+	Comments
+	)
+	set exerciseId = (select id from Exercises where exerciseName = @exerciseName),
+	categoryId = (select id from Categories where categoryName = @categoryName)
+	;
 
